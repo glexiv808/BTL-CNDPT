6e65a3bdc7e37ca387d674f89e3ad360c066d3fe
nguyen <125894255+glexiv808@users.noreply.github.com>
2024-11-08 00:45:38 +0700

    nguyen commit

--- footer.js
+++ footer.js
@@ -1,12 +1,12 @@
 let appFooter = `
     <nav>
            <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-white py-5 px-sm-3 px-lg-5" style="margin-top: 90px;">
+    <div class="container-fluid bg-darkblue text-white py-5 px-sm-3 px-lg-5" style="margin-top: 90px;">
         <div class="row pt-5">
             <div class="col-lg-7 col-md-12">
                 <div class="row">
                     <div class="col-md-6 mb-5">
-                        <h5 class="text-primary text-uppercase mb-4" style="letter-spacing: 5px;">Liên hệ</h5>
+                        <h5 class="text-purple text-uppercase mb-4" style="letter-spacing: 5px;">Liên hệ</h5>
                         <p><i class="fa fa-map-marker-alt mr-2"></i>Đồng Tâm, Hai Bà Trưng, Hà Nội</p>
                         <p><i class="fa fa-phone-alt mr-2"></i>+012 345 67890</p>
                         <p><i class="fa fa-envelope mr-2"></i>info@example.com</p>
@@ -18,7 +18,7 @@ let appFooter = `
                         </div>
                     </div>
                     <div class="col-md-6 mb-5">
-                        <h5 class="text-primary text-uppercase mb-4" style="letter-spacing: 5px;">Các khóa học</h5>
+                        <h5 class="text-purple text-uppercase mb-4" style="letter-spacing: 5px;">Các khóa học</h5>
                         <div class="d-flex flex-column justify-content-start">
                             <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Chứng khoán</a>
                             <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Phân tích thị trường</a>
@@ -28,23 +28,23 @@ let appFooter = `
                 </div>
             </div>
             <div class="col-lg-5 col-md-12 mb-5">
-                <h5 class="text-primary text-uppercase mb-4" style="letter-spacing: 5px;">Thông tin mới</h5>
+                <h5 class="text-purple text-uppercase mb-4" style="letter-spacing: 5px;">Thông tin mới</h5>
                 <p>Để lại email để nhận được cập nhật những thông tin mới nhất về khóa học và khuyến mãi cũng như những thông tin tài chính mới nhất </p>
                 <div class="w-100">
                     <div class="input-group">
                         <input type="text" class="form-control border-light" style="padding: 30px;" placeholder="Email của bạn">
                         <div class="input-group-append">
-                            <button class="btn btn-primary px-4">Đăng kí ngay</button>
+                            <button class="btn bg-purple px-4 text-white">Đăng kí ngay</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="container-fluid bg-dark text-white border-top py-4 px-sm-3 px-md-5" style="border-color: rgba(256, 256, 256, .1) !important;">
+    <div class="container-fluid bg-darkblue text-white border-top py-4 px-sm-3 px-md-5" style="border-color: rgba(256, 256, 256, .1) !important;">
         <div class="row">
             <div class="col-lg-6 text-center text-md-left mb-3 mb-md-0">
-                <p class="m-0 text-white">&copy; <a href="#">Domain Name</a>.Trang thông tin điện tử Trung tâm NCKH&ĐTCK: Giấy phép Số 126/GP-TTĐT, Bộ Thông tin và Truyền thông cấp ngày 20/7/2020.
+                <p class="m-0 text-white">&copy; <a class="text-purple" href="#">Domain Name</a>.Trang thông tin điện tử Trung tâm NCKH&ĐTCK: Giấy phép Số 126/GP-TTĐT, Bộ Thông tin và Truyền thông cấp ngày 20/7/2020.
                 </p>
             </div>
             <div class="col-lg-6 text-center text-md-right">
